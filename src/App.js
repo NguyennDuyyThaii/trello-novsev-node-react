ceebc0ae58b95456b59d268f208a05125608a67d
Nguyen Duy Thai <thaind@techvify.com.vn>
2022-03-20 01:23:29 +0700

    design trello

--- src/App.js
+++ src/App.js
@@ -1,9 +1,342 @@
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
-    <div className="App">
-      
+    <div className="trello-master">
+      <nav className="navbar app">App Bar</nav>
+      <nav className="navbar board">Board Bar</nav>
+      <nav className="board-columns">
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+        <div className="column">
+          <header>
+            Brainstorm
+          </header>
+          <ul>
+            <li>
+              <img src="https://i1.wp.com/stevenmortimer.com/blog/5-steps-to-change-github-default-branch-from-master-to-main/steps-1-to-3-create-main-and-point-HEAD.png?zoom=2&w=578&ssl=1" alt="" />
+              title: Thai
+            </li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+            <li>Nguyen duy thai .....</li>
+          </ul>
+          <footer>
+            Add another card
+          </footer>
+        </div>
+      </nav>
     </div>
   );
 }
